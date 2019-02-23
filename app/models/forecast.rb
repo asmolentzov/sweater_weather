c270f1e26d80c0ea9cb13e7c858faf11b1e53940
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-23 12:46:57 -0700

    Adds date to Forecast

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -2,12 +2,16 @@ class Forecast < ApplicationRecord
   validates_presence_of(:city)
   validates_presence_of(:state)
   
-  before_create :get_lat_long
+  before_create :set_lat_long, :set_date
   
   private
   
-  def get_lat_long
+  def set_lat_long
     self.latitude = LocationService.new(city, state).get_latitude
     self.longitude = LocationService.new(city, state).get_longitude
+  end
+  
+  def set_date
+    self.date = Time.now.strftime('%Y-%m-%d')
   end
 end
