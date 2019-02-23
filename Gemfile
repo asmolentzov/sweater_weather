2e35b2ecfa6e9852e7dd9a1346de586754d674bc
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-23 10:46:11 -0700

    Adds shoulda-matchers and model test for forecast

--- Gemfile
+++ Gemfile
@@ -55,6 +55,7 @@ end
 
 group :test do
   gem 'simplecov'
+  gem 'shoulda-matchers'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Forecast, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:city) }
+  it { should validate_presence_of(:state) }
 end
