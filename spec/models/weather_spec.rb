c32dc6af519c1f4aec6be1f1dadd706ed523d8d0
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-23 14:41:06 -0700

    Adds test for current weather day

--- spec/models/weather_spec.rb
+++ spec/models/weather_spec.rb
@@ -10,4 +10,20 @@ describe Weather do
     expect(weather.latitude).to eq(lat)
     expect(weather.longitude).to eq(long)
   end
+  
+  it 'can return current weather day info' do
+    lat = '12'
+    long = '-12'
+    weather = Weather.new(lat, long)
+    current_weather_day_info = {
+                                temp_feels_like: 30,
+                                humidity: 0.75,
+                                visibility: 10.00,
+                                uv_index: 2,
+                                summary: 'Mostly cloudy until afternoon',
+                                summary_short: 'Partly Cloudy',
+                                summary_tonight: 'Partly cloudy starting tonight, continuing until tomorrow morning'
+    }
+    expect(weather.current_weather_day).to eq(current_weather_day_info)
+  end
 end
