77a016fdf8b473fc7057c697f129f72842ddd43c
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-23 11:04:31 -0700

    Adds get_lat_long initial method

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,4 +1,13 @@
 class Forecast < ApplicationRecord
   validates_presence_of(:city)
   validates_presence_of(:state)
+  
+  before_create :get_lat_long
+  
+  private
+  
+  def get_lat_long
+    self.latitude = LocationService.new(city, state).get_latitude
+    self.longitude = LocationService.new(city, state).get_longitude
+  end
 end
--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -10,9 +10,12 @@ RSpec.describe Forecast, type: :model do
     describe 'get_lat_long' do
       it 'gets the latitude and longitude for a forecast' do
         forecast = Forecast.create(city: 'Denver', state: 'CO')
-        
         expect(forecast.latitude).to eq('39.7392')
         expect(forecast.longitude).to eq('104.9903')
+        
+        hawaii_forecast = Forecast.create(city: 'Honolulu', state: 'HI')
+        expect(hawaii_forecast.latitude).to eq('21.3069')
+        expect(hawaii_forecast.longitude).to eq('157.8583')
       end
     end
   end
