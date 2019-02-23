6af1cd5677a7f7f0806fdb4f2db08126328b144b
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-23 15:00:41 -0700

    Adds test for getting weather

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -10,4 +10,22 @@ describe WeatherService do
     expect(weather_service.latitude).to eq(lat)
     expect(weather_service.longitude).to eq(long)
   end
+  it 'can get weather' do
+    lat = '12'
+    long = '-12'
+    weather_service = WeatherService.new(lat, long)
+    
+    weather_data = weather_service.get_weather
+    expect(weather_data).to have_key(:daily)
+    expect(weather_data).to have_key(:hourly)
+    expect(weather_data).to have_key(:currently)
+    expect(weather_data[:currently]).to have_key(:apparentTemperature)
+    expect(weather_data[:currently]).to have_key(:humidity)
+    expect(weather_data[:hourly]).to have_key(:summary)
+    expect(weather_data[:hourly][:data].first).to have_key(:temperature)
+    expect(weather_data[:daily][:data].first).to have_key(:icon)
+    expect(weather_data[:daily][:data].first).to have_key(:precipProbability)
+    expect(weather_data[:daily][:data].first).to have_key(:temperatureLow)
+    expect(weather_data[:daily][:data].first).to have_key(:temperatureHigh)
+  end
 end
