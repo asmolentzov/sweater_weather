e9df2ca0197db8534c34f6a96c9a966c97e92c17
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-23 13:56:51 -0700

    Updates serializer and tests with forecast being poro

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,4 +1,5 @@
 class ForecastSerializer
   include FastJsonapi::ObjectSerializer
+  set_id :object_id
   attributes :city, :state, :latitude, :longitude, :date
 end
--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -4,26 +4,23 @@ describe 'Forecast API' do
   it 'gets the lat/long from the city/state requested' do
     get '/api/v1/forecast?location=denver,co'
     
-    forecast = Forecast.last
-    
+    forecast_data = JSON.parse(response.body, symbolize_names: true)
+    denver_lat = '39.7392358'
+    denver_long = '-104.990251'
     expect(response).to be_successful
-    expect(forecast.city).to eq('Denver')
-    expect(forecast.state).to eq('CO')
-    expect(forecast.latitude).to eq('39.7392358')
-    expect(forecast.longitude).to eq('-104.990251')
+    expect(forecast_data[:data][:attributes][:city]).to eq('Denver')
+    expect(forecast_data[:data][:attributes][:state]).to eq('CO')
+    expect(forecast_data[:data][:attributes][:latitude]).to eq(denver_lat)
+    expect(forecast_data[:data][:attributes][:longitude]).to eq(denver_long)
   end
   
   it 'returns weather data' do
     get '/api/v1/forecast?location=denver,co'
     
-    forecast = Forecast.last
     date = Time.now.strftime('%Y-%m-%d')
     expect(response).to be_successful
     forecast_data = JSON.parse(response.body, symbolize_names: true)
-    expect(forecast_data[:data][:attributes][:latitude]).to eq(forecast.latitude)
-    expect(forecast_data[:data][:attributes][:longitude]).to eq(forecast.longitude)
-    expect(forecast_data[:data][:attributes][:city]).to eq(forecast.city)
-    expect(forecast_data[:data][:attributes][:state]).to eq(forecast.state)
+
     expect(forecast_data[:data][:attributes][:date]).to eq(date)
     expect(forecast_data[:data]).to have_key(:current_weather_day)  
     expect(forecast_data[:data]).to have_key(:weather_day)  
