4d73d129548883acaa91fdaf44dc8fed5504a722
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-23 11:13:33 -0700

    Adds tests for getting lat and long for a location service

--- spec/services/location_service_spec.rb
+++ spec/services/location_service_spec.rb
@@ -9,6 +9,17 @@ describe LocationService do
     expect(location_service.state).to eq("CO")
   end
   it 'can get latitude for a city/state' do
+    location_service = LocationService.new("Denver", "CO")
+    expec(location_service.get_latitude).to eq('39.7392')
     
+    location_service = LocationService.new("Honolulu", "HI")
+    expec(location_service.get_latitude).to eq('21.3069')
+  end
+  it 'can get longitude for a city/state' do
+    location_service = LocationService.new("Denver", "CO")
+    expec(location_service.get_longitude).to eq('104.9903')
+    
+    location_service = LocationService.new("Honolulu", "HI")
+    expec(location_service.get_longitude).to eq('157.8583')
   end
 end
