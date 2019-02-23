9204f341b0873029a325184d785e3d019245f20d
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-23 11:42:29 -0700

    Adds get_longitude method and refactors

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -6,14 +6,22 @@ class LocationService
   end
   
   def get_latitude
+    get_location[:lat].to_s
+  end
+  
+  def get_longitude
+    get_location[:lng].to_s
+  end
+  
+  private
+  
+  def get_location
     response = conn.get do |f|
       f.params[:address] = "#{@city},#{@state}"
     end
     data = JSON.parse(response.body, symbolize_names: true)
-    data[:results].first[:geometry][:location][:lat].to_s
+    data[:results].first[:geometry][:location]
   end
-  
-  private
   
   def conn
     Faraday.new(url: 'https://maps.googleapis.com/maps/api/geocode/json') do |faraday|
