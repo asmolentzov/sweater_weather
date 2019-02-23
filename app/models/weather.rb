d6f8a5499199fb555edd481523aab380bcd7dfc5
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-23 14:49:50 -0700

    Adds current_weather_day method to weather class

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -6,4 +6,18 @@ class Weather
     @latitude = latitude
     @longitude = longitude
   end
+  
+  def current_weather_day
+    weather_data = WeatherService.new(@latitude, @longitude).get_weather
+    today_weather = weather_data[:daily][:data].first
+    {
+      temp_feels_like: weather_data[:currently][:apparentTemperature],
+      humidity: weather_data[:currently][:humidity],
+      visibility: weather_data[:currently][:visibility],
+      uv_index: weather_data[:currently][:uvIndex],
+      summary_short: weather_data[:currently][:summary],
+      summary: today_weather[:summary],
+      summary_tonight: today_weather[:summary]
+    }
+  end
 end
