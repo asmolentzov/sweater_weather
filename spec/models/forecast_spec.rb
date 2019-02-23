46a8d64f09890bcb1a56a8766c14e64f09657ec0
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-23 10:47:53 -0700

    Adds validations to Forecast

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Forecast, type: :model do
-  it { should validate_presence_of(:city) }
-  it { should validate_presence_of(:state) }
+  describe 'Validations' do
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+  end
 end
