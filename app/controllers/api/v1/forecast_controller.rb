8613664b56c224392111da6e733cfcf51d2f0b9a
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-23 10:51:41 -0700

    Adds forecast creation to controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::ForecastController < ApplicationController
   def index
+    forecast = Forecast.create(forecast_params)
     render json: 'hi'
+  end
+  
+  private
+  
+  def forecast_params
+    location = params[:location].split(',')
+    { city: location[0].capitalize, state: location[1].upcase }
   end
 end
