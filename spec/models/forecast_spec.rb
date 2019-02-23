9bb411c80282d8517eedb9596186912f6f662a24
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-23 10:59:36 -0700

    Adds model test for callback for forecast setting lat/long

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe Forecast, type: :model do
     it { should validate_presence_of(:city) }
     it { should validate_presence_of(:state) }
   end
+  
+  describe 'Callbacks' do
+    describe 'get_lat_long' do
+      it 'gets the latitude and longitude for a forecast' do
+        forecast = Forecast.create(city: 'Denver', state: 'CO')
+        
+        expect(forecast.latitude).to eq('39.7392')
+        expect(forecast.longitude).to eq('104.9903')
+      end
+    end
+  end
 end
