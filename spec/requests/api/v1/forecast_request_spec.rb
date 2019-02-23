b79869d3a45ac1988eca1f8d7677e53a10f431db
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-23 10:22:43 -0700

    Adds request test for a get forecast request to get lat/long

---
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'Forecast API' do
+  it 'gets the lat/long from the city/state requested' do
+    get '/api/v1/forecast?location=denver,co'
+    
+    forecast = Forecast.last
+    
+    expect(forecast.city).to eq('Denver')
+    expect(forecast.state).to eq('CO')
+    expect(forecast.latitude).to eq('39.7392')
+    expect(forecast.longitude).to eq('104.9903')
+  end
+end
