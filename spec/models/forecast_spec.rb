02a62226f1c67c55cfa2289e258b3268923c4b8e
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-23 12:45:13 -0700

    Adds test for setting date upon creation of forecast

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe Forecast, type: :model do
   end
   
   describe 'Callbacks' do
-    describe 'get_lat_long' do
-      it 'gets the latitude and longitude for a forecast' do
+    describe 'set_lat_long' do
+      it 'sets the latitude and longitude for a forecast' do
         forecast = Forecast.create(city: 'Denver', state: 'CO')
         expect(forecast.latitude).to eq('39.7392358')
         expect(forecast.longitude).to eq('-104.990251')
@@ -16,6 +16,13 @@ RSpec.describe Forecast, type: :model do
         hawaii_forecast = Forecast.create(city: 'Honolulu', state: 'HI')
         expect(hawaii_forecast.latitude).to eq('21.3069444')
         expect(hawaii_forecast.longitude).to eq('-157.8583333')
+      end
+    end
+    describe 'set_date' do
+      it 'sets the date for a forecast' do
+        forecast = Forecast.create(city: 'Denver', state: 'CO')
+        
+        expect(forecast.date).to eq(Time.now.strftime('%Y-%m-%d'))
       end
     end
   end
