57828d4897488cb1ff0a71290869b084e723003f
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-24 13:43:21 -0700

    Adds test for creating api key

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -14,4 +14,18 @@ describe 'Users API' do
     expect(user.email).to eq(email)
     expect(user.password_digest).to_not be_nil
   end
+  it 'returns an api key when it creates a user' do
+    email = 'whatever@example.com'
+    password = 'password'
+    post '/api/v1/users', params: { 
+                                  email: email, 
+                                  password: password, 
+                                  password_confirmation: password
+                                }
+    expect(response.status).to eq(201)
+    result = JSON.parse(response.body)
+    expect(result).to have_key(:api_key)
+    user = User.last
+    expect(user.api_key).to eq(result[:api_key])
+  end
 end
