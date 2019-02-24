b6cf100fdc6b63a743df94192bbbda21eb1cf99c
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-24 12:03:40 -0700

    Updates/refactors get image method

--- app/services/image_service.rb
+++ app/services/image_service.rb
@@ -1,25 +1,36 @@
 class ImageService
   
   attr_reader :city, :state
+  Project_weather_group_id = '1463451@N25'
   
   def initialize(city, state)
     @city = city
     @state = state
   end
   
-  def get_image(group_id = '1463451@N25')
-    response = conn.get('/services/rest') do |f|
+  def get_image
+    "https://farm#{random_image[:farm]}.staticflickr.com/#{random_image[:server]}/#{random_image[:id]}_#{random_image[:secret]}_b.jpg"
+  end
+  
+  private
+  
+  def random_image
+    @random_image ||= get_image_list[:photo][random_image_number]
+  end
+  
+  def get_image_list(group_id = Project_weather_group_id)
+    @response ||= conn.get('/services/rest') do |f|
       f.params[:method] = 'flickr.groups.pools.getPhotos'
       f.params[:group_id] = group_id
       f.params[:tags] = "#{@city},#{@state}"
     end
-    image_list = JSON.parse(response.body, symbolize_names: true)
-    image_count = image_list[:photos][:total].to_i
-    image_number = rand(0...image_count)
-    image_info = image_list[:photos][:photo][image_number]
+    JSON.parse(@response.body, symbolize_names: true)[:photos]
   end
   
-  private
+  def random_image_number
+    image_count = get_image_list[:total].to_i
+    rand(0...image_count)
+  end
   
   def conn
     Faraday.new(url: 'https://api.flickr.com') do |faraday|
