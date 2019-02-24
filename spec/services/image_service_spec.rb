7a2459f56fe7ce69d4d439fd948e9651fb63d649
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-24 12:03:56 -0700

    Updates test to check for images from two cities

--- spec/services/image_service_spec.rb
+++ spec/services/image_service_spec.rb
@@ -10,9 +10,20 @@ describe ImageService do
     expect(image_service.city).to eq(city)
     expect(image_service.state).to eq(state)
   end
-  it 'can get an image' do
+  it 'can get an image', :vcr do
     city = 'denver'
     state = 'co'
+    image_service = ImageService.new(city, state)
+    
+    image_url = image_service.get_image
+    uri = URI.parse(image_url)
+    expect(uri.scheme).to eq('https')
+    expect(uri.host.split('.')[1]).to eq('staticflickr')
+    expect(uri.host.split('.')[2]).to eq('com')
+  end
+  it 'can get an image from another city', :vcr do
+    city = 'honolulu'
+    state = 'hi'
     image_service = ImageService.new(city, state)
     
     image_url = image_service.get_image
