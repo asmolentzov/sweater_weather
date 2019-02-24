1e736340a12d528974cbe6284a21926552eb695d
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-24 13:56:48 -0700

    Adds test for api_key creation on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,13 @@ class User < ApplicationRecord
   validates_presence_of :password, require: true
   
   has_secure_password
+  
+  before_create :set_api_key
+  
+  private
+  
+  def set_api_key
+    self.api_key = SecureRandom.hex(14)
+  end
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,12 @@ describe User do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
   end
+  
+  describe 'Callbacks' do
+    it 'should add api_key on user creation' do
+      user = User.create(email: 'user@example.com', password: 'password')
+      
+      expect(user.api_key).to_not be_nil
+    end
+  end
 end
