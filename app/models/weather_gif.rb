aa3aa877eff5ba75d6f4382a726a6bb908068942
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-25 10:02:14 -0700

    Adds giphy api request and gets url

--- app/models/weather_gif.rb
+++ app/models/weather_gif.rb
@@ -4,4 +4,19 @@ class WeatherGif
     @time = Time.new(date).to_i
     @summary = summary
   end
+  
+  def url
+    conn = Faraday.new('https://api.giphy.com') do |faraday|
+      faraday.params[:api_key] = ENV['giphy_api_key']
+      faraday.adapter Faraday.default_adapter
+    end
+    
+    response = conn.get('/v1/gifs/search') do |f|
+      f.params[:q] = summary
+      f.params[:limit] = 1
+    end
+    
+    raw_gifs = JSON.parse(response.body, symbolize_names: true)
+    raw_gifs[:data].first[:images][:fixed_width][:url]
+  end
 end
--- spec/models/weather_gif_spec.rb
+++ spec/models/weather_gif_spec.rb
@@ -10,13 +10,14 @@ describe WeatherGif do
     expect(weather_gif.time).to eq(Time.new(date).to_i)
     expect(weather_gif.summary).to eq(summary)
   end
-  it 'has a gif url' do
+  it 'has a gif url', :vcr do
     date = '2019-02-25'
     summary = 'Partly cloudy through the afternoon'
     weather_gif = WeatherGif.new(date, summary)
     
     expect(weather_gif.url).to be_a(String)
     uri = URI.parse(weather_gif.url)
-    expect(uri.host).to eq('media0.giphy.com')
+    expect(uri.host.split('.').second).to eq('giphy')
+    expect(uri.host.split('.').last).to eq('com')
   end
 end
