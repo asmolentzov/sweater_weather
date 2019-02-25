94af82488e770622ab8baa48174e4a57c37f0929
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-24 17:40:43 -0700

    Adds request test for sessions endpoint

--- spec/factories/forecasts.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :forecast do
-    city { "MyString" }
-    state { "MyString" }
-    latitude { "MyString" }
-    longitude { "MyString" }
-  end
-end
---
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'Sessions API' do
+  it 'returns an API key when it receives a POST request' do
+    user = User.create(email: 'email@example.com', password: 'password')
+    
+    post '/api/v1/sessions'
+    
+    expect(response.status).to eq(200)
+    result = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(result).to have_key(:api_key)
+    expect(result[:api_key]).to eq(user.api_key)
+  end
+end
