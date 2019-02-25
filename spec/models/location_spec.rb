caf76a87581e062ced44246d1a248b820b3b0051
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-25 14:32:30 -0700

    Adds model test for location

---
+++ spec/models/location_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe Location do
+  describe 'Validations' do
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:latitude) }
+    it { should validate_presence_of(:longitude) }
+  end
+end
