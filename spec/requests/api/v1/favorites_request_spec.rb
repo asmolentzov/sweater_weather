7082f1310e90421063bed1c207893dbb2c00525e
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-24 19:32:21 -0700

    Adds test for returning 401 with wrong API key

--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -5,6 +5,17 @@ describe 'Favorites API' do
     post '/api/v1/favorites', params: { location: 'Denver, CO' }
     
     expect(response.status).to eq(401)
-    expect(response.body).to be_nil
+    expect(response.body).to eq('{}')
+  end
+  it 'returns 401 if making post request with incorrect API key' do
+    user = User.create(email: 'email', password: 'password')
+    
+    post '/api/v1/favorites', params: {
+                                        location: 'Denver, CO',
+                                        api_key: 'not an api key'
+                                      }
+    
+    expect(response.status).to eq(401)
+    expect(response.body).to eq('{}')
   end
 end
