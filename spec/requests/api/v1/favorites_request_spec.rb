0902481fbc80cd2c6629a4030dbd3b63c4f7c03c
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-24 19:17:27 -0700

    Adds request test for favorites API requiring api key

---
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe 'Favorites API' do
+  it 'returns 401 if making post request with no API key' do
+    post '/api/v1/favorites', params: { location: 'Denver, CO' }
+    
+    expect(response.status).to eq(401)
+    expect(response.body).to be_nil
+  end
+end
