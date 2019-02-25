e99fa907ebc8788f3bbcd18aac51770973a5da20
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-25 14:07:58 -0700

    Adds request test for getting favorites list

--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -30,4 +30,41 @@ describe 'Favorites API' do
     expect(favorite.location).to eq(location)
     expect(favorite.user).to eq(user)
   end
+  it 'returns a list of a users favorites - empty if no favorites' do
+    user = User.create(email: 'email', password: 'password')
+    get '/api/v1/favorites', params: { api_key: user.api_key }
+    
+    expect(response.status).to eq(200)
+    favorites = JSON.parse(response.body, symbolize_names: true)
+    expect(favorites).to be_a(Array)
+    expect(favorites).to be_empty
+  end
+  it 'returns a list of a users favorites' do
+    user = User.create(email: 'email', password: 'password')
+    location = 'Denver, CO'
+    location_2 = 'Honolulu, HI'
+    user.favorites.create(location: location)
+    user.favorites.create(location: location_2)
+    get '/api/v1/favorites', params: { api_key: user.api_key }
+    
+    expect(response.status).to eq(200)
+    favorites = JSON.parse(response.body, symbolize_names: true)
+    expect(favorites).to be_a(Array)
+    expect(favorites.first).to be_a(Hash)
+    expect(favorites.first).to have_key(:location)
+    expect(favorites.first[:location]).to eq(location)
+    expect(favorites.first).to have_key(:current_weather)
+    expect(favorites.first[:current_weather]).to have_key(:temperature)
+    expect(favorites.first[:current_weather]).to have_key(:temp_feels_like)
+    expect(favorites.first[:current_weather]).to have_key(:temp_high)
+    expect(favorites.first[:current_weather]).to have_key(:temp_low)
+    expect(favorites.first[:current_weather]).to have_key(:humidity)
+    expect(favorites.first[:current_weather]).to have_key(:visibility)
+    expect(favorites.first[:current_weather]).to have_key(:uv_index)
+    expect(favorites.first[:current_weather]).to have_key(:summary)
+    expect(favorites.first[:current_weather]).to have_key(:summary_short)
+    expect(favorites.first[:current_weather]).to have_key(:summary_tonight)
+    expect(favorites.last[:location]).to eq(location_2)
+    expect(favorites.last).to have_key(:current_weather)
+  end
 end
