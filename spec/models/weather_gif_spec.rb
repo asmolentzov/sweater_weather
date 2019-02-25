88e39cca882417d1ab0a2e9acd708fe42886bbc0
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-25 09:26:08 -0700

    Adds basic test for having images

--- spec/models/weather_gif_spec.rb
+++ spec/models/weather_gif_spec.rb
@@ -7,4 +7,13 @@ describe WeatherGif do
     expect(weather_gif).to be_a(WeatherGif)
     expect(weather_gif.location).to eq(location)
   end
+  
+  it 'has images' do
+    location = 'denver,co'
+    weather_gif = WeatherGif.new(location)
+    
+    expect(weather_gif.images).to be_a(Array)
+    expect(weather_gif.images.count).to eq(5)
+    # expect(weather_gif.images.first).to be_a(Gif)
+  end
 end
