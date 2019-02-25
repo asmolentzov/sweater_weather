afb322b22505bcc31480b7f4538641b29e105ae2
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-25 10:09:16 -0700

    Adds test for get_gif method

--- spec/services/gif_service_spec.rb
+++ spec/services/gif_service_spec.rb
@@ -5,4 +5,14 @@ describe GifService do
     service = GifService.new
     expect(service).to be_a(GifService)
   end
+  
+  it 'can get gif url', :vcr do
+    summary = 'partly cloudy through evening'
+    service = GifService.new
+    gif_url = service.get_gif(summary)
+    expect(gif_url).to be_a(String)
+    uri = URI.parse(gif_url)
+    expect(uri.host.split('.').second).to eq('giphy')
+    expect(uri.host.split('.').last).to eq('com')
+  end
 end
